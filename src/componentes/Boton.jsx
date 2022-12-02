b3073af4bfe6f5b436b7ef4b41772353869418dd
pablo.mandile@bue.edu.ar <pablo.mandile@bue.edu.ar>
2022-12-02 16:30:07 -0300

    componentes botones y distinción entre operadores u otros botones

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import '../css/Boton.css';
+
+const esOperador = valor =>{
+   return isNaN(valor) && (valor != '.') && (valor != '=');
+};
 
 function Boton(props) {
   return (
-    <div>
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
       {props.children}
     </div>
   );
